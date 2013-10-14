ba0a405b19d524ec5cbfae50ddfbbb4a09eae1a9
hhqcontinue <hhqcontinue@gmail.com>
2013-10-14 10:07:04 +0800

    fix a bug in authority

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,15 +8,17 @@ import models.*;
 public class Application extends Controller {
     public static void index() {
         String userName = "";
+        int authority = 0;
         if(session.get("userId") != null) {
             String userId = session.get("userId");
             User user = User.find("byUserid",userId).first();
             userName = user.name;
+            authority = user.authority;
         }
     	List<Upload> olderUploads = Upload.find("order by postedAt desc").from(0).fetch(3);
     	List<News> latestNews = News.findAll();
     	Report report = Report.find("order by id desc").first();
-    	render(olderUploads,latestNews,report,userName);
+    	render(olderUploads,latestNews,report,userName, authority);
     }
 
     public static void about() {
